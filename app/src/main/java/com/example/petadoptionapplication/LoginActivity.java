98f83dee3f8aafb3ee5f89245ddc08735657d3f5
Tea <stefanovskatea@gmail.com>
2021-07-08 10:34:52 +0200

    Global Constants class

--- app/src/main/java/com/example/petadoptionapplication/LoginActivity.java
+++ app/src/main/java/com/example/petadoptionapplication/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -35,6 +34,8 @@ public class LoginActivity extends AppCompatActivity {
         }
         else{
             ((TextView)findViewById(R.id.logintitle)).setText("Incorrect username or password...");
+            ((TextView)findViewById(R.id.usernameField)).setText("");
+            ((TextView)findViewById(R.id.passwordField)).setText("");
         }
     }
 
--- app/src/main/java/com/example/petadoptionapplication/MockCredentials.java
+++ app/src/main/java/com/example/petadoptionapplication/MockCredentials.java
@@ -1,8 +1,25 @@
 package com.example.petadoptionapplication;
 
 public class MockCredentials {
+
+    //adopters
     public static final String username1 = "Tea";
-    protected static final String username2 = "Tea";
-    protected static final String password1 = "Tea";
-    protected static final String password2 = "Tea";
+    public static final String password1 = "Tea";
+
+    //personnel
+    public static final String password2 = "Tea";
+    public static final String username2 = "Tea";
+
+
+    //animals
+
+    //dogs
+    public static final String animalRex = "Rex";
+    public static final String animalMax = "Max";
+    //cats
+    public static final String animalSparky = "Sparky";
+    //parrot
+    public static final String animalChio = "Chio";
+
+
 }
